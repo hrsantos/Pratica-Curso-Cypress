086fabd32f7e23719c19c14e402d9b09677d45ac
Haroldo R Santos <haroldo.rsantos@gmail.com>
2025-01-07 11:32:01 -0300

    TASKS - implementação de testes independentes e alteração para utilização de contantes na manipulação da massa de teste

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,11 +1,12 @@
 /// <reference types="cypress" />
 
 //método para definir uma suite de testes 
-describe('Tarefas', ()=> {
+describe('Tarefas', () => {
 
     // método para definir um caso de teste
-    it('Deve cadastrar uma nova tarefa', ()=> {
+    it('Deve cadastrar uma nova tarefa', () => {
 
+        const taskName ='Ler um livro de node JS'
         //PREPARAR A MASSA DE TESTE: verifica via api se existe uma tarefa registrada
         // e remove a tarefa se existir
 
@@ -13,7 +14,7 @@ describe('Tarefas', ()=> {
         cy.request({
             url: 'http://localhost:3333/helper/tasks',
             method: 'DELETE',
-            body: {name: 'Ler um livro de node JS'}
+            body: { name: taskName }
         }).then(response => {
             expect(response.status).to.eq(204)
         })
@@ -22,14 +23,51 @@ describe('Tarefas', ()=> {
         cy.visit('http://localhost:3000')
 
         //STEP busca o elemento do tipo input e preenche com um valor
-        cy.get('input[placeholder="Add a new Task"]').type('Ler um livro de node JS')
+        cy.get('input[placeholder="Add a new Task"]').type(taskName)
 
         //STEP busca o elemento botão CREATE e clica nele (XPATH button[text() = 'Create '])
-        cy.contains('button','Create').click()
+        cy.contains('button', 'Create').click()
 
-        //ASSERTION verifica via mapeamendo de css selector se o elemento contém o texto esperado
+        //STEP verifica via mapeamendo de css selector se o elemento contém o texto esperado
         // e verifica se o texto está visível 
-        cy.contains('main div p', 'Ler um livro de node JS')
+        cy.contains('main div p', taskName)
             .should('be.visible')
+    })
+
+    it('Não deve permitir o cadastro de duas tarefas iguais', () => {
+       
+        const task = {
+            name: 'Estudar JS',
+            is_done: false
+        }
+
+        /*CRIAÇÃO DA MASSA VIA API DE FORMA A MANTER A INDEPENDÊNCIA ENTRE OS CENÁRIOS DE TESTES
+        necessita de correção no servidor
+        cy.request({
+            url: 'http://localhost:3333/tasks',
+            method: 'POST',
+            body: { name: 'Estudar JS', is_done: false}
+        }).then(response => {
+            expect(response.status).to.eq(201)
+        })*/
+
+        //STEP Criação da 1ª Tarefa
+        cy.visit('http://localhost:3000');
+
+        cy.get('input[placeholder="Add a new Task"]').type(task.name)
+
+        cy.contains('button', 'Create').click()
+
+        //STEP Criação da 2ª Tarefa
+        cy.visit('http://localhost:3000');
+
+        cy.get('input[placeholder="Add a new Task"]').type(task.name)
+
+        cy.contains('button', 'Create').click()
+
+        //STEP verifica se existe uma tarefa igual já cadastrada
+        cy.get('.swal2-html-container')
+            .should('be.visible')
+            .should('have.text', 'Task already exists!')
     })
 })
