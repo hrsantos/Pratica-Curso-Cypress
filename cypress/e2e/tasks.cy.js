ca0f5808ea7eb015d658f7724fd701e85a4dd835
Haroldo R Santos <haroldo.rsantos@gmail.com>
2025-01-07 16:25:14 -0300

    TASKS - aplicação de organização dos testes por contexto

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -3,50 +3,53 @@
 //método para definir uma suite de testes 
 describe('Tarefas', () => {
 
-    // método para definir um caso de teste
-    it('Deve cadastrar uma nova tarefa', () => {
+    //organização dos testes por contexto
+    context('Cadastro', () => {
+        // método para definir um caso de teste
+        it('Deve cadastrar uma nova tarefa', () => {
 
-        const nomeTarefa ='Ler um livro de node JS'
+            const nomeTarefa = 'Ler um livro de node JS'
 
-        //exclui uma tarefa de mesmo nome caso ela já exista
-        cy.excluirTarefa(nomeTarefa)
+            //exclui uma tarefa de mesmo nome caso ela já exista
+            cy.excluirTarefa(nomeTarefa)
 
-        //cria nova tarefa
-        cy.criarTarefa(nomeTarefa)
+            //cria nova tarefa
+            cy.criarTarefa(nomeTarefa)
 
-        /*verifica via mapeamendo de css selector se o elemento contém o texto esperado
-        e verifica se o texto está visível*/ 
-        cy.contains('main div p', nomeTarefa)
-            .should('be.visible')
-    })
+            /*verifica via mapeamendo de css selector se o elemento contém o texto esperado
+            e verifica se o texto está visível*/
+            cy.contains('main div p', nomeTarefa)
+                .should('be.visible')
+        })
 
-    it('Não deve permitir o cadastro de duas tarefas iguais', () => {
-       
-        //criação de objeto de valor constante
-        const tarefa = {
-            name: 'Estudar JS',
-            is_done: false
-        }
+        it('Não deve permitir o cadastro de duas tarefas iguais', () => {
 
-        /*CRIAÇÃO DA MASSA VIA API DE FORMA A MANTER A INDEPENDÊNCIA ENTRE OS CENÁRIOS DE TESTES
-        necessita de correção no servidor*/
-        //cy.criarTarefaAPI(tarefa)
+            //criação de objeto de valor constante
+            const tarefa = {
+                name: 'Estudar JS',
+                is_done: false
+            }
 
-        //criação da 1ª Tarefa
-        cy.criarTarefa(tarefa.name)
+            /*CRIAÇÃO DA MASSA VIA API DE FORMA A MANTER A INDEPENDÊNCIA ENTRE OS CENÁRIOS DE TESTES
+            necessita de correção no servidor*/
+            //cy.criarTarefaAPI(tarefa)
 
-        //criação da 2ª Tarefa
-        cy.criarTarefa(tarefa.name)
+            //criação da 1ª Tarefa
+            cy.criarTarefa(tarefa.name)
 
-        //verifica se existe uma tarefa igual já cadastrada
-        cy.get('.swal2-html-container')
-            .should('be.visible')
-            .should('have.text', 'Task already exists!')
-    })
+            //criação da 2ª Tarefa
+            cy.criarTarefa(tarefa.name)
 
-    it('Campo Add new task deve ser obrigatório', () => {
+            //verifica se existe uma tarefa igual já cadastrada
+            cy.get('.swal2-html-container')
+                .should('be.visible')
+                .should('have.text', 'Task already exists!')
+        })
 
-        cy.criarTarefa()
-        cy.ValidaPreechimentoObrigatorio('This is a required field')
+        it('Campo Add new task deve ser obrigatório', () => {
+
+            cy.criarTarefa()
+            cy.ValidaPreechimentoObrigatorio('This is a required field')
+        })
     })
 })
